b935c1673183bb550dd441b44cdd4df733d5489d
Abulela44 <abulelamanisa@gmail.com>
2023-06-20 15:38:08 -0700

    544

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle," followed by newline.
- * return 0.
+ * main - prints "Programming is like building a multilingual puzzle,"
+ * followed by newline.
+ * Return: Always 0
  */
-
 int main(void)
-
 {
-
-puts("Programming is like building a multilingual puzzle.\n");
- 
-return(0);
+puts("\"Programming is like building a multilingual puzzle.\n");
+return (0);
 
 }
