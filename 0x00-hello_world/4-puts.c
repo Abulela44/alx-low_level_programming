107fa5836a21951809793d4a47dca790433ccafb
Abulela44 <abulelamanisa@gmail.com>
2023-06-20 12:52:52 -0700

    4and5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle," followed by a new line.
+ * main - prints "Programming is like building a multilingual puzzle," followed by newline.
  * return 0.
  */
 
 int main(void)
 
 {
-  puts("\"Programming is like building a multilingual puzzleh.\n");
+
+puts("Programming is like building a multilingual puzzle.\n");
  
-  return(0);
- 
+return(0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,"  followed by a new line
+ * main - prints "with proper grammar, but the outcome is a piece of art,"  followed by newline
  *
  * Return: Always 0.
  *
  */
  
- int main(void)
+int main(void)
  
 {
 printf("with proper grammar, but the outcome is a piece of art,\n"); 
 
-	Return (0);
+return(0);
 
 }
