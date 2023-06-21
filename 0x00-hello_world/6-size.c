e5d219fa261a445b8a1dcbd3f521a0217af65401
Abulela44 <abulelamanisa@gmail.com>
2023-06-21 16:34:29 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("Size of s char: %c\n", sizeofbyte(s));
+printf("Size of an int: %d\n", sizeofbyte(s));
+printf("Size of a long int: %ld\n", sizeofbyte(s));
+printf("Size of a long long int: %lld\n", sizeofbyte(s));
+printf("Size of a float: %f\n", sizeofbyte(s));
+return (0);
+}
