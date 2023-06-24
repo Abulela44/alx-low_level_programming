bcce3ec1ce27a3dcb0e6fed751dd808f1753b9ef
Abulela44 <abulelamanisa@gmail.com>
2023-06-24 07:28:35 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char L26;
+for (L26 = 'a'; L26 <= 'z'; L26++)
+putchar(L26);
+putchar('\n');
+return 0;
+}
