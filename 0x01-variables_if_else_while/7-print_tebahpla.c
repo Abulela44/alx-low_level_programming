cb34ba96d433073387f8fdc5b0de811cae11e4c7
Abulela44 <abulelamanisa@gmail.com>
2023-06-24 12:32:28 -0700

    ./9-print_comb.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char l26;
+for (l26 = 'z'; l26 <= 'a'; l26--)
+putchar(l26);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+/**
+ *  * main - Entry point
+ *   * Return: Always 0
+ *    */
+
+int main(void)
+{
+int i;
+char l6;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+for (l6 = 'a'; l6 <= 'f'; l6++)
+putchar(l6);
+putchar('\n');
+return (0);
+}
