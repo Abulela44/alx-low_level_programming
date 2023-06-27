d88ed03e7febaa3878ff86f588d6171ee0f7f9ee
Abulela44 <abulelamanisa@gmail.com>
2023-06-27 09:41:46 -0700

    0-reset_to_98.cv

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98.c - Give the value of pointer points at
+ * reset_to_98.c - Give the value of pointer points to 98 
+ *
  * @n: int input pointer
- * Return: Nothing
+ *
+ * Return : Nothing
+ *
  */
 void reset_to_98(int *n)
 {
